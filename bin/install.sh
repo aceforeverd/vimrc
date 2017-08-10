89bfaeaa9b2c863f193102a60582da8041f29d91
aceforeverd <aceforever@protonmail.com>
2017-08-11 04:43:25 +0800

    fall back

--- bin/install.sh
+++ bin/install.sh
@@ -46,6 +46,8 @@ function check_commands {
 function install_vim_plug {
     if [ ! -d "$VIMPLUG_PATH" ]; then
         mkdir -p "$VIMPLUG_PATH"
+    elif [ -f "${VIMPLUG_PATH}/plug.vim" ] ; then
+        rm "${VIMPLUG_PATH}/plug.vim"
     fi
 
     "$WGET" "$VIMPLUG" -O "$VIMPLUG_PATH"/plug.vim && \
@@ -56,6 +58,8 @@ function install_vim_plug {
 function install_dein {
     if [ ! -d "$DEIN_PATH" ]; then
         mkdir -p "$DEIN_PATH"
+    else
+        rm -rf "$DEIN_PATH"
     fi
 
     "$GIT" clone "$DEIN_URL" "$DEIN_PATH" && \
@@ -66,20 +70,37 @@ function install_dein {
 function install_vimrc {
     if [ ! -d "$VIMRC_PATH" ] ; then
         mkdir -p "$VIMRC_PATH"
+    else
+        rm -rf "$VIMRC_PATH"
     fi
 
     "$GIT" clone "$VIMRC_URL" "$VIMRC_PATH" && \
         echo "installed vimrc at $VIMRC_PATH"
     echo ""
 
-    echo "source ~/.vim_runtime/vimrc" >> ~/.vimrc
+    if [ -f $HOME/.vimrc ] ; then
+        mv $HOME/.vimrc $HOME/.vimrc-backup
+        echo "moved your old vimrc file to $HOME/.vimrc-backup"
+    fi
+
+    echo "source ~/.vim_runtime/vimrc" > ~/.vimrc
+
+    echo "=================================================================================="
+    echo "installing plugins from vim-plug"
+    echo "=================================================================================="
+    vim -c "PlugInstall | q" && \
+        echo "finished"
+    echo "=================================================================================="
+
+    echo "=================================================================================="
+    echo "installing plugins from dein"
+    vim -c "call dein#install() | q" && \
+        echo "finished"
+    echo "=================================================================================="
+
 }
 
-function lanuch_vim {
-    vim -c 'call dein#install() | PlugInstall '
-}
 
 install_vim_plug
 install_dein
 install_vimrc
-lanuch_vim
