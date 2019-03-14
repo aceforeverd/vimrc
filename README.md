cdf095ca0e3ceea06ae27a4af5030f0252bf1747
aceforeverd <teapot@aceforeverd.com>
2019-03-14 20:04:18 +0800

    rewrite install procedure
    readme
    setup.sh

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-# My vim configuration
-
-## Installation
+# My vim config
+---
+## Requirements
++ vim >= 8 or neovim >= 0.3
++ git
+---
+## Install
+### clone
 ```bash
-curl https://raw.githubusercontent.com/aceforeverd/vimrc/master/scripts/install.sh | bash
+git clone https://github.com/aceforeverd/vimrc.git "$CLONE_PATH"
+```
+where `$CLONE_PATH` is `$HOME/.vim` for vim and `$HOME/.config/nvim` for neovim
+
+### set up plugin managers
+run `setup.sh` inside clone path
+```bash
+cd "$CLONE_PATH"
+./scripts/setup.sh
 ```
 
-
-## Note
-This should work in vim 8 with most features enabled.
-Some plugin requre extra dependency, e.g. clang, go, ruby, rust, etc.
+### install plugins
+1. open vim/neovim
+2. for vim plug, type
+    ```vimscript
+    :PlugInstall
+    ```
+3. for dein.vim, type
+    ```vimscript
+    :call dein#install()
+    ```
+### enjoy
+---
+## Notes
+### plugin managers used:
+1. [vim plug](https://github.com/junegunn/vim-plug.git)
+2. [dein.vim](https://github.com/Shougo/dein.vim.git)
---
+++ scripts/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# File              : setup.sh
+# Date              : 13.03.2019
+# Last Modified Date: 13.03.2019
+
+
+# the script set up vim plugin manager for vim/neovim:
+# 1. dein.vim (https://github.com/Shougo/dein.vim.git)
+# 2. vimplug (https://github.com/junegunn/vim-plug.git)
+
+set -eE
+set -o nounset
+
+cd "$(dirname "$0")"
+cd ..
+
+ROOT=$(pwd)
+DEIN_PATH=$ROOT/dein/repos/github.com/Shougo/dein
+VIMPLUG_PATH=$ROOT/autoload
+DEIN_URL="https://github.com/Shougo/dein.vim.git"
+VIMPLUG="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
+if [ ! -d "$DEIN_PATH" ] ; then
+    mkdir -p "$DEIN_PATH"
+fi
+
+echo -e "\033[0;32mStart setting up plugin managers (vim plug & dein.vim)\033[0m"
+
+echo -e "\033[0;32mInstalling vim plug ...\033[0m"
+curl -fLo "$VIMPLUG_PATH/plug.vim" --create-dirs "$VIMPLUG" && \
+    echo -e "\033[1;32mvim plug installed as $VIMPLUG_PATH/plug.vim\033[0m"
+echo ""
+
+echo -e "\033[0;32mInstalling dein.vim ...\033[0m"
+git clone "$DEIN_URL" "$DEIN_PATH" && \
+    echo -e "\033[1;32mdein.vim installed at $DEIN_PATH\033[0m"
+echo ""
+
+echo -e "\033[1;32mPlugin managers all setted"
