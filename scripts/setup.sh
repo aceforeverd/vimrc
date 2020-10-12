d4409a2e61bf6bfec71f128b82a80eda3083c31c
aceforeverd <teapot@aceforeverd.com>
2020-10-13 00:33:33 +0800

    setup.sh: install plugins inside script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -79,6 +79,9 @@ echo -e "\033[1;32mPlugin managers all setted"
 
 if [[ $TYPE = "neovim" ]] ; then
     ln -s "$ROOT/vimrc.vim" "$HOME/.config/nvim/init.vim"
+    nvim -E -c "call dein#install()" -c "PlugInstall" -c 'q'
 elif [[ $TYPE = "vim" ]] ; then
     ln -s "$ROOT/vimrc.vim" "$HOME/.vim/vimrc"
+    vim -E -c "call dein#install()" -c "PlugInstall" -c 'q'
 fi
+echo -e "\033[1;32mPlugins installed"
