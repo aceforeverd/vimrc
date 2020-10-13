24a81d5e33dd1554715b5ca7fb02982249415df6
aceforeverd <teapot@aceforeverd.com>
2020-10-13 14:34:52 +0800

    Run plug install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -78,11 +78,8 @@ echo ""
 echo -e "\033[1;32mPlugin managers all setted"
 
 if [[ $TYPE = "neovim" ]] ; then
-    # nvim -u "$ROOT/vimrc.vim" -E -c "call dein#install()" -c "PlugInstall" -c 'q'
     ln -s "$ROOT/vimrc.vim" "$ROOT/init.vim"
 elif [[ $TYPE = "vim" ]] ; then
-    # vim -u "$ROOT/vimrc.vim" -E -c "call dein#install()" -c "PlugInstall" -c 'q'
-    # ln -s "$ROOT/vimrc.vim" "$HOME/.vim/vimrc"
     echo
 fi
 echo -e "\033[1;32mPlugins installed"
