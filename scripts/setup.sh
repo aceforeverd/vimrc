77b635c278b73451d56073d2476bd28a65b15c67
aceforeverd <teapot@aceforeverd.com>
2020-10-15 20:33:26 +0800

    enable PlugUpdate in setup script (#4)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -91,10 +91,14 @@ if [[ -n "$INSTALL_PLUGINS" ]]; then
         ln -s vimrc.vim init.vim
         echo -e "${GREEN}installing dein plugins for neovim${NC}"
         nvim --headless -u "$ROOT/vimrc.vim" -c "call dein#install()" -c "qa!"
+        nvim --headless -u "$ROOT/vimrc.vim" -c "PlugUpdate" -c 'qa!'
+        echo -e "${GREEN}all plugins installed${NC}"
     else
         ln -s vimrc.vim vimrc
         echo -e "${GREEN}installing dein plugins for vim${NC}"
-        vim -E -c "set t_ti= t_te= nomore" -u "$ROOT/vimrc.vim" -c "call dein#install()" -c "qa!"
+        vim -c "set t_ti= t_te= nomore" -u "$ROOT/vimrc.vim" -c "call dein#install()" -c "qa!"
+        vim -c "set t_ti= t_te= nomore" -S "$ROOT/vimrc.vim" -c "PlugUpdate" -c "qa!"
+        echo -e "${GREEN}all plugins installed${NC}"
     fi
 fi
 popd
