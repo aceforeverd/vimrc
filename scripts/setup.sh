84544e4f20d2d6aea5c7b9ae1a524a1928f6bcdb
aceforeverd <teapot@aceforeverd.com>
2020-10-16 03:28:42 +0800

    fix dein install cause the dein merge directory is affected by init file name

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -90,14 +90,14 @@ if [[ -n "$INSTALL_PLUGINS" ]]; then
     if [[ "$TYPE" = "neovim" ]]; then
         ln -s vimrc.vim init.vim
         echo -e "${GREEN}installing dein plugins for neovim${NC}"
-        nvim --headless -u "$ROOT/vimrc.vim" -c "call dein#install()" -c "qa!"
-        nvim --headless -u "$ROOT/vimrc.vim" -c "PlugUpdate" -c 'qa!'
+        nvim --headless -u init.vim -c "call dein#install()" -c "qa!"
+        nvim --headless -u init.vim -c "PlugUpdate" -c 'qa!'
         echo -e "${GREEN}all plugins installed${NC}"
     else
         ln -s vimrc.vim vimrc
         echo -e "${GREEN}installing dein plugins for vim${NC}"
-        vim -c "set t_ti= t_te= nomore" -u "$ROOT/vimrc.vim" -c "call dein#install()" -c "qa!"
-        vim -c "set t_ti= t_te= nomore" -S "$ROOT/vimrc.vim" -c "PlugUpdate" -c "qa!"
+        vim -c "set t_ti= t_te= nomore" -u vimrc -c "call dein#install()" -c "qa!"
+        vim -c "set t_ti= t_te= nomore" -u vimrc -c "PlugUpdate" -c "qa!"
         echo -e "${GREEN}all plugins installed${NC}"
     fi
 fi
