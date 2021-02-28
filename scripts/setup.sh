7d40cf46079b69d637bf6d804431eacd32f21c64
aceforeverd <teapot@aceforeverd.com>
2021-02-28 17:48:50 +0800

    try

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -105,7 +105,7 @@ if [[ -n "$INSTALL_PLUGINS" ]]; then
         ln -s vimrc.vim vimrc
         echo -e "${GREEN}installing dein plugins for vim${NC}"
         vim -c "set t_ti= t_te= nomore" -u vimrc -c "call dein#install()" -c "echomsg 'dein plugins installed'" -c "qa!"
-        vim -c "set t_ti= t_te= nomore" -S vimrc -c "CocUpdateSync" -c "echomsg 'coc plugins installed'" -c "qa!"
+        vim -c "set t_ti= t_te= nomore" -S vimrc -c "runtime! plugin/**/*.vim" -c "CocUpdateSync" -c "echomsg 'coc plugins installed'" -c "qa!"
         vim -c "set t_ti= t_te= nomore" -S vimrc -c "PlugUpdate" -c "echomsg 'vim-plug plugins installed'" -c "qa!"
         echo -e "${GREEN}all plugins installed${NC}"
     fi
