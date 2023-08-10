0bef9721b05841da05fe36d705d63565c15e52ea
aceforeverd <teapot@aceforeverd.com>
2023-08-10 10:03:33 +0800

    add missing file

---
+++ lua/aceforeverd/config/init.lua
@@ -0,0 +1,5 @@
+return {
+  teelscope = require('aceforeverd.config.finder').telescope,
+  cmp = require('aceforeverd.config.cmp').setup,
+  treesitter = require('aceforeverd.config.treesitter').setup
+}
