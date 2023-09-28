fe73af3cf518497b40f6d6037fc77adced64c38a
aceforeverd <teapot@aceforeverd.com>
2023-09-28 14:25:02 +0800

    feat: migrate to indent-blankline v3

--- lua/aceforeverd/plugins/enhance.lua
+++ lua/aceforeverd/plugins/enhance.lua
@@ -60,7 +60,7 @@ end
 function M.substitute()
   require('substitute').setup({
     on_substitute = function(event)
-      require("yanky.integration").substitute()
+      require('yanky.integration').substitute()
     end,
     yank_substituted_text = true,
   })
@@ -129,22 +129,21 @@ function M.indent_blankline()
   vim.opt.list = true
   vim.opt.listchars:append('space:⋅')
 
-  require('indent_blankline').setup({
-    char_list = { '|', '¦', '┆', '┊' },
-    space_char_blankline = ' ',
-    show_trailing_blankline_indent = false,
-    buftype_exclude = { 'terminal' },
-    filetype_exclude = {
-      'startify',
-      'coc-explorer',
-      'NvimTree',
-      'help',
-      'git',
-      'packer',
-      'lsp-installer',
-      'mason.nvim',
-      'OverseerForm',
-      'alpha',
+  require('ibl').setup({
+    indent = {
+      char = { '▎', '¦', '┆', '┊' },
+    },
+    exclude = {
+      filetypes = {
+        'startify',
+        'coc-explorer',
+        'NvimTree',
+        'help',
+        'git',
+        'mason',
+        'OverseerForm',
+        'alpha',
+      },
     },
   })
 end
