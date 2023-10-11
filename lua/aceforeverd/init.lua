4b6805bbeac456f4e38f7a429201971bdc2af6bc
aceforeverd <teapot@aceforeverd.com>
2023-10-11 21:51:31 +0800

    fix(fillchar): foldclose to the right unicode

--- lua/aceforeverd/init.lua
+++ lua/aceforeverd/init.lua
@@ -41,7 +41,7 @@ function M.setup()
   vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
   vim.o.foldlevelstart = 99
   vim.o.foldenable = true
-  vim.o.fillchars = [[foldopen:▼,foldclose:⏵]]
+  vim.o.fillchars = [[foldopen:▼,foldclose:▶]]
   vim.o.exrc = true -- automatically execute project local configs
 
   vim.filetype.add({
